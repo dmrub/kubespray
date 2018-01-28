e4c86fb619907f15a59d63a642ba2043fc2cb3eb
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2018-01-28 23:28:08 +0100

    Improved scripts.

--- bin/run-script.sh
+++ bin/run-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-THIS_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
+THIS_DIR=$(dirname "$( readlink -f "${BASH_SOURCE}" 2>/dev/null || \
+  python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE}" )")
 
 source "$THIS_DIR/init-env.sh"
 
