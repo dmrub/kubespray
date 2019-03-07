1437f4ca442881302806ac12043608664c7aed5a
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-03-07 14:38:28 +0100

    Update add-user.sh utility

--- bin/add-user.sh
+++ bin/add-user.sh
@@ -1,138 +1,154 @@
 #!/bin/bash
 
-script() {
-    local username=$1
-    local user_groups=$2
-    local password=$3
+THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P))
 
-    local tool
-    for tool in useradd groupadd getent usermod; do
-        if ! type -f "$tool" > /dev/null 2>&1; then
-            echo >&2 "No $tool tool found"
-            exit 1
-        fi
-    done
+# shellcheck source=init-env.sh
+source "$THIS_DIR/init-env.sh"
 
-    if ! getent passwd "$username"; then
-        # Create user
-        if useradd -m "$username"; then
-            if [ -n "$password" ]; then
-                # Set password
-                passwd --stdin "$username" <<<"$password"
-            fi
-        fi
-    fi
-
-    if ! getent passwd "$username" >/dev/null; then
-        echo >&2 "Could not create user $username"
-        exit 1
-    fi
-
-    user_groups=${user_groups//,/ }
-
-    local grp
-    for grp in $user_groups; do
-        echo "Add user $username to group $grp"
-        if ! getent group "$grp" >/dev/null; then
-            groupadd "$grp" || {
-                echo >&2 "Could not create group $grp"
-                exit 1
-            }
-        fi
-        usermod -a -G "$grp" "$username" || {
-            echo >&2 "Could not add $username user to $grp group"
-            exit 1
-        }
-    done
+usage() {
+    echo "Add user"
+    echo
+    echo "$0 [options] user-name [password]"
+    echo "options:"
+    echo "  -p, --host-pattern=         Ansible host pattern"
+    echo "  -g, --groups=               Groups separated by comma"
+    echo "  -k, --user-public-key-file= User public key file"
+    echo "      --help                  Display this help and exit"
+    echo "      --                      End of options"
 }
 
-if [ "$1" != "--remote" ]; then
-    THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
+# https://www.linuxjournal.com/content/normalizing-path-names-bash
 
-    # shellcheck source=init-env.sh
-    source "$THIS_DIR/init-env.sh"
+normpath() {
+    # Remove all /./ sequences.
+    local path=${1//\/.\//\/}
 
-    usage() {
-        echo "Add user"
-        echo
-        echo "$0 [options] user-name [password]"
-        echo "options:"
-        echo "  -p, --host-pattern=        Ansible host pattern"
-        echo "  -g, --groups               Groups separated by comma"
-        echo "      --help                 Display this help and exit"
-        echo "      --                     End of options"
+    # Remove dir/.. sequences.
+    while [[ $path =~ ([^/][^/]*/\.\./) ]]; do
+        path=${path/${BASH_REMATCH[0]}/}
+    done
+    echo "$path"
+}
+
+if test -x /usr/bin/realpath; then
+    abspath() {
+        if [[ -d "$1" || -d "$(dirname "$1")" ]]; then
+            /usr/bin/realpath "$1"
+        else
+            case "$1" in
+                "" | ".") echo "$PWD";;
+                /*) normpath "$1";;
+                *)  normpath "$PWD/$1";;
+            esac
+        fi
     }
+else
+    abspath() {
+        if [[ -d "$1" ]]; then
+            (cd "$1" && pwd)
+        else
+            case "$1" in
+                "" | ".") echo "$PWD";;
+                /*) normpath "$1";;
+                *)  normpath "$PWD/$1";;
+            esac
+        fi
+    }
+fi
 
-    HOST_PATTERN=all
-    USER_GROUPS=
+USER_GROUPS=
+USER_PASSWORD=
+HOST_PATTERN=
+USER_PUBLIC_KEY_FILE=
 
-    while [[ "$1" == "-"* ]]; do
-        case "$1" in
-            -p|--host-pattern)
-                HOST_PATTERN="$2"
-                shift 2
-                ;;
-            --host-pattern=*)
-                HOST_PATTERN="${1#*=}"
-                shift
-                ;;
-            -g|--groups)
-                USER_GROUPS="${USER_GROUPS},$2"
-                shift 2
-                ;;
-            --groups=*)
-                USER_GROUPS="${USER_GROUPS},${1#*=}"
-                shift
-                ;;
-            --help)
-                usage
-                exit
-                ;;
-            --)
-                shift
-                break
-                ;;
-            -*)
-                fatal "Unknown option $1"
-                ;;
-            *)
+while [[ "$1" == "-"* ]]; do
+    case "$1" in
+        -p|--host-pattern)
+            HOST_PATTERN="$2"
+            shift 2
+            ;;
+        --host-pattern=*)
+            HOST_PATTERN="${1#*=}"
+            shift
+            ;;
+        -g | --groups)
+            USER_GROUPS="${USER_GROUPS}${USER_GROUPS:+,}$2"
+            shift 2
+            ;;
+        --groups=*)
+            USER_GROUPS="${USER_GROUPS}${USER_GROUPS:+,}${1#*=}"
+            shift
+            ;;
+        -k|--user-public-key-file)
+            USER_PUBLIC_KEY_FILE="$2"
+            shift 2
+            ;;
+        --user-public-key-file=*)
+            USER_PUBLIC_KEY_FILE="${1#*=}"
+            shift
+            ;;
+        --help)
+            usage
+            exit
+            ;;
+        --)
+            shift
             break
             ;;
-        esac
-    done
+        -*)
+            fatal "Unknown option $1"
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
 
-    if [ -n "$1" ]; then
-        USERNAME=$1
-        shift
-    else
-        fatal "No username specified"
-    fi
-
-    if [ -n "$1" ]; then
-        PASSWORD=$1
-        shift
-    else
-        echo "No password specified, password will be not set"
-    fi
-
-    USER_GROUPS=${USER_GROUPS// /,}
-
-    echo "Add user $USERNAME to $USER_GROUPS groups"
-    if [ -n "$PASSWORD" ]; then
-        echo "And set password"
-    fi
-
-    THIS_SCRIPT="$( readlink -f "${BASH_SOURCE[0]}" 2>/dev/null || \
-  python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE[0]}" )"
-
-    run-ansible \
-            "$HOST_PATTERN" \
-            --become \
-            -m script \
-            --args "$THIS_SCRIPT --remote '$USERNAME' '$USER_GROUPS' '$PASSWORD'"
-
-else
-    # Remote code
+if [[ -n "$1" ]]; then
+    USERNAME=$1
     shift
-    script "$@"
+else
+    fatal "No username specified"
 fi
+
+if [[ -n "$1" ]]; then
+    USER_PASSWORD=$1
+    shift
+else
+    echo "No password specified, password will be not set"
+fi
+
+USER_GROUPS=${USER_GROUPS// /,}
+
+echo "Add user '$USERNAME' to $USER_GROUPS groups"
+if [[ -n "$USER_PASSWORD" ]]; then
+    echo "And set user password"
+fi
+
+USER_PUBLIC_KEY_FILE=$(abspath "$USER_PUBLIC_KEY_FILE")
+
+jsquote() {
+    local arg=$1
+    arg=${arg//\"/\\\"}
+    echo "\"${arg}\""
+}
+
+EXTRA_VARS="{\"user_name\": $(jsquote "$USERNAME"), \"user_groups\": $(jsquote "$USER_GROUPS")"
+if [[ -n "$USER_PASSWORD" ]]; then
+    EXTRA_VARS="$EXTRA_VARS, \"user_password\": $(jsquote "$USER_PASSWORD")"
+fi
+if [[ -n "$USER_PUBLIC_KEY_FILE" ]]; then
+    EXTRA_VARS="$EXTRA_VARS, \"user_public_key_file\": $(jsquote "$USER_PUBLIC_KEY_FILE")"
+fi
+EXTRA_VARS="${EXTRA_VARS}"'}'
+
+OPTS=(--extra-vars "$EXTRA_VARS")
+if [[ -n "$HOST_PATTERN" ]]; then
+    OPTS+=(-l "$HOST_PATTERN")
+fi
+
+NO_ECHO=yes
+run-ansible-playbook \
+    "${OPTS[@]}" \
+    "$ANSIBLE_PLAYBOOKS_DIR/add-user.yml" \
+    "$@"
