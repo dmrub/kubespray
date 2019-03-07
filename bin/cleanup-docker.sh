cdb240be1bb641c21ae70a77f024d0b589f528bb
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-03-07 14:47:08 +0100

    Improve cleanup-docker.sh script

--- bin/cleanup-docker.sh
+++ bin/cleanup-docker.sh
@@ -3,4 +3,4 @@
 THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 
 # shellcheck disable=SC2016
-"$THIS_DIR/run-shell.sh" -b 'DP=$(docker ps -q -f status=exited) && [ -n "$DP" ] && docker rm $DP || true; DI=$(docker images -f "dangling=true" -q) && [ -n "$DI" ] && docker rmi $DI || true;'
+"$THIS_DIR"/run-shell.sh -b 'for FL in status=exited status=dead; do echo "Remove containers with $FL "; X=$(docker ps -q -f $FL) && [ -n "$X" ] && docker rm $X || true; done; DI=$(docker images -f "dangling=true" -q) && [ -n "$DI" ] && docker rmi $DI || true;'
