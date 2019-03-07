8a420b0a20b4b0d918ad105a71aed9a823ca2859
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-03-07 14:47:18 +0100

    Improve scale-cluster.sh script

--- bin/scale-cluster.sh
+++ bin/scale-cluster.sh
@@ -7,4 +7,10 @@ source "$THIS_DIR/init-env.sh"
 
 set -e
 
+run-ansible-playbook "$ANSIBLE_PLAYBOOKS_DIR/disable-firewall.yml" "$@"
+
+run-ansible-playbook "$ANSIBLE_PLAYBOOKS_DIR/disable-swap.yml" "$@"
+
+run-ansible all -m setup
+
 run-ansible-playbook "$ANSIBLE_PLAYBOOKS_DIR/scale.yml" --become --become-user=root "$@"
