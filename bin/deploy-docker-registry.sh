990eec2ed05854cfe90e069df61846a52cecc8b4
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-05-02 12:08:31 +0200

    When deploying docker registry don't fail when setup for all nodes fails

--- bin/deploy-docker-registry.sh
+++ bin/deploy-docker-registry.sh
@@ -5,8 +5,8 @@ THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-set -e
+set -eo pipefail
 
-run-ansible all -m setup
+run-ansible all -m setup || true;
 
 run-ansible-playbook "$ANSIBLE_PLAYBOOKS_DIR/deploy-docker-registry.yml" "$@"
