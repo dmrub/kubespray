19eafff839b6b0551c2ec770a7e3b438d1a6d666
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-09-30 10:56:01 +0200

    Improve scripts and add run-ansible-inventory.sh script
    Abort shell scripts immediately if errors occur
    Simplify initialization code

--- bin/run-ansible-inventory.sh
+++ bin/run-ansible-inventory.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -eo pipefail
-
 THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
+
+set -eo pipefail
 
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-"$THIS_DIR/configure.py" "$@"
+run-ansible-inventory "$@"
