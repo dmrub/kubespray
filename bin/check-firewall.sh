6f58240fde5f6cd6bd7403fbbc4f510c60c17b2a
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-10-02 12:41:12 +0200

    Improve configuration scripts and remove unused scripts

--- bin/check-firewall.sh
+++ bin/check-firewall.sh
@@ -5,6 +5,6 @@ THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-ansible -i "$ANSIBLE_INVENTORY" \
+run-ansible \
         --become \
         all -m shell --args "firewall-cmd --state"
