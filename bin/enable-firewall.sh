559b9d184831d6e8764e13a7d0427589b4271da8
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-10-02 12:41:12 +0200

    Improve configuration scripts and remove unused scripts

--- bin/enable-firewall.sh
+++ bin/enable-firewall.sh
@@ -5,7 +5,7 @@ THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-ansible all -v -i "$ANSIBLE_INVENTORY" \
+run-ansible all -v \
         --become \
         -m shell \
         --args 'systemctl enable firewalld; systemctl start firewalld'
