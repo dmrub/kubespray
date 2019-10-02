3d8966df5a80485e672e834c8be60583d7a326da
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-10-02 12:41:12 +0200

    Improve configuration scripts and remove unused scripts

--- bin/check-connectivity.sh
+++ bin/check-connectivity.sh
@@ -2,8 +2,16 @@
 
 THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 
-set -e
+set -eo pipefail
 
-IFS=" " read -r -a NODE_PORTS <<< "$("$THIS_DIR/kubectl.sh" get svc netchecker-service -o go-template  --template '{{ range .spec.ports}}{{ .nodePort }} {{ end }}')"
+echo >&2 "+ $THIS_DIR/kubectl.sh get svc netchecker-service -o go-template  --template '{{ range .spec.ports}}{{ .nodePort }} {{ end }}'"
+OUTPUT=$("$THIS_DIR/kubectl.sh" get svc netchecker-service -o go-template  --template '{{ range .spec.ports}}{{ .nodePort }} {{ end }}')
+
+IFS=" " read -r -a NODE_PORTS <<< "$OUTPUT"
+
+if [[ "${#NODE_PORTS[@]}" -eq 0 ]]; then
+    echo >&2 "FATAL: No netchecker-service node ports found !"
+    exit 1
+fi
 
 "$THIS_DIR/run-ansible.sh" k8s-cluster -m uri -a "url=http://127.0.0.1:${NODE_PORTS[0]}/api/v1/connectivity_check return_content=yes"
--- bin/init-env.sh
+++ bin/init-env.sh
@@ -6,17 +6,13 @@ CFG_CONFIG_FILE_DIR=$(dirname "$CFG_CONFIG_FILE")
 ANSIBLE_DIR=$ROOT_DIR
 ANSIBLE_PLAYBOOKS_DIR=$ANSIBLE_DIR
 ANSIBLE_INVENTORY_DIR=$ROOT_DIR/inventory
-ANSIBLE_INVENTORY=$ANSIBLE_INVENTORY_DIR/inventory.cfg
+CFG_ANSIBLE_INVENTORIES=("$ANSIBLE_INVENTORY_DIR/inventory.cfg")
 
 CFG_CONFIG_DIR=${CFG_CONFIG_DIR:-~/.ansible}
 CFG_VAULT_FILE=${CFG_VAULT_FILE:-""}
 CFG_VARS_FILE=${CFG_VARS_FILE:-""}
 
 export ANSIBLE_PRIVATE_KEY_FILE=${ANSIBLE_PRIVATE_KEY_FILE:-~/.ssh/cluster_id_rsa}
-export ANSIBLE_VAULT_PASSWORD_FILE=${ANSIBLE_VAULT_PASSWORD_FILE:-${CFG_CONFIG_DIR}/vault_pass.txt}
-if [[ ! -e "$ANSIBLE_VAULT_PASSWORD_FILE" ]]; then
-    unset ANSIBLE_VAULT_PASSWORD_FILE
-fi
 export ANSIBLE_CONFIG=$ANSIBLE_DIR/ansible.cfg
 export ANSIBLE_FILTER_PLUGINS=$ANSIBLE_DIR/filter_plugins
 export ANSIBLE_ROLES_PATH=$ANSIBLE_DIR/roles
@@ -35,15 +31,32 @@ abspath() {
         python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "${1}"
 }
 
+join_by() {
+  local IFS="$1"
+  shift
+  echo "$*"
+}
+
 print-info() {
     echo "Current Configuration:"
     echo "Configuration file:            $(abspath "$CFG_CONFIG_FILE")"
     echo
-    echo "Ansible inventory file:        $(abspath "$ANSIBLE_INVENTORY")"
-    echo "Config directory:              $(abspath "$CFG_CONFIG_DIR")"
-    echo "Ansible vault password file:   $(abspath "$ANSIBLE_VAULT_PASSWORD_FILE")"
-    echo "Ansible remote user:           $ANSIBLE_REMOTE_USER"
-    echo "Ansible private SSH key file:  $(abspath "$ANSIBLE_PRIVATE_KEY_FILE")"
+
+    local inventories inventory vault_password_files vault_password_file
+    inventories=()
+    for inventory in "${CFG_ANSIBLE_INVENTORIES[@]}"; do
+      inventories+=( "$(abspath "$inventory")" )
+    done
+    vault_password_files=()
+    for vault_password_file in "${CFG_ANSIBLE_VAULT_PASSWORD_FILES[@]}"; do
+        vault_password_files+=( "$(abspath "$vault_password_file")" )
+    done
+
+    echo "Ansible inventory file(s):        $(join_by ", " "${inventories[@]}")"
+    echo "Config directory:                 $(abspath "$CFG_CONFIG_DIR")"
+    echo "Ansible vault password file(s):   $(join_by ", " "${vault_password_files[@]}")"
+    echo "Ansible remote user:              $ANSIBLE_REMOTE_USER"
+    echo "Ansible private SSH key file:     $(abspath "$ANSIBLE_PRIVATE_KEY_FILE")"
     echo
 }
 
@@ -51,9 +64,9 @@ check-config() {
     if [[ ! -d "$CFG_CONFIG_DIR" ]]; then
         fatal "Configuration directory does not exist, run $(abspath "$THIS_DIR/configure.sh")"
     fi
-    if [[ ! -e "$ANSIBLE_VAULT_PASSWORD_FILE" ]]; then
-        fatal "Ansible vault password file does not exist, run $(abspath "$THIS_DIR/configure.sh")"
-    fi
+    #if [[ ! -e "$ANSIBLE_VAULT_PASSWORD_FILE" ]]; then
+    #    fatal "Ansible vault password file does not exist, run $(abspath "$THIS_DIR/configure.sh")"
+    #fi
     #if [[ -z "$ANSIBLE_REMOTE_USER" ]]; then
     #    fatal "Remote user name is not configured, run $THIS_DIR/configure.sh"
     #fi
@@ -82,11 +95,19 @@ ansible_playbook() {
     ansible-playbook -i "$@"
 }
 
-add-inventory-to-opts() {
+add-inventories-to-opts() {
     # opts array is defined outside
-    if [[ -n "$ANSIBLE_INVENTORY" ]]; then
-        opts+=(-i "$ANSIBLE_INVENTORY")
-    fi
+    local inventory
+    for inventory in "${CFG_ANSIBLE_INVENTORIES[@]}"; do
+      opts+=( -i "$inventory" )
+    done
+}
+
+add-vault-config-to-opts() {
+    local vault_password_file
+    for vault_password_file in "${CFG_ANSIBLE_VAULT_PASSWORD_FILES[@]}"; do
+        opts+=( "--vault-password-file=$vault_password_file" )
+    done
 }
 
 add-remote-user-to-opts() {
@@ -125,7 +146,8 @@ $*"
 run-ansible() {
     local opts
     opts=()
-    add-inventory-to-opts
+    add-inventories-to-opts
+    add-vault-config-to-opts
     add-remote-user-to-opts
     add-extra-vars-to-opts
 
@@ -135,19 +157,30 @@ run-ansible() {
 run-ansible-inventory() {
     local opts
     opts=()
-    add-inventory-to-opts
+    add-inventories-to-opts
+    add-vault-config-to-opts
     # remote user and extra vars are not supported by ansible-inventory
 
     run-ansible-cmd ansible-inventory "$@"
 }
 
+run-ansible-vault() {
+    local opts
+    opts=()
+    # inventories, remote users and extra vars are not supported by ansible-vault
+    add-vault-config-to-opts
+
+    run-ansible-cmd ansible-vault "$@"
+}
+
+
 run-ansible-playbook() {
     check-config
-    check-inventory "$ANSIBLE_INVENTORY"
 
     local opts
     opts=()
-    add-inventory-to-opts
+    add-inventories-to-opts
+    add-vault-config-to-opts
     add-remote-user-to-opts
     add-extra-vars-to-opts
 
@@ -184,15 +217,3 @@ CFG_SHELL_CONFIG=$("$THIS_DIR/configure.py" --shell-config)
 
 eval "$CFG_SHELL_CONFIG"
 
-#if [[ -e "$CFG_CONFIG_FILE" ]]; then
-#    read-config-file "$CFG_CONFIG_FILE" "CFG_"
-#    export ANSIBLE_REMOTE_USER=${CFG_ANSIBLE_REMOTE_USER:-$ANSIBLE_REMOTE_USER}
-#    export ANSIBLE_PRIVATE_KEY_FILE=${CFG_ANSIBLE_PRIVATE_KEY_FILE:-$ANSIBLE_PRIVATE_KEY_FILE}
-#    export ANSIBLE_INVENTORY=${CFG_ANSIBLE_INVENTORY:-$ANSIBLE_INVENTORY}
-#    if [[ "$ANSIBLE_INVENTORY" != /* ]]; then
-#        ANSIBLE_INVENTORY=$(abspath "${CFG_CONFIG_FILE_DIR}/${ANSIBLE_INVENTORY}")
-#    fi
-#    if [[ "$ANSIBLE_PRIVATE_KEY_FILE" != /* ]]; then
-#        ANSIBLE_PRIVATE_KEY_FILE=$(abspath "${CFG_CONFIG_FILE_DIR}/${ANSIBLE_PRIVATE_KEY_FILE}")
-#    fi
-#fi
