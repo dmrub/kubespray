9154b9ff3821f8efe70a8efc5fd7acc80c960c62
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-10-02 12:41:12 +0200

    Improve configuration scripts and remove unused scripts

--- bin/check-iptables.sh
+++ bin/check-iptables.sh
@@ -5,6 +5,6 @@ THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-ansible all -i "$ANSIBLE_INVENTORY" \
+run-ansible all \
         --become \
         -m shell --args 'iptables -t nat -S DOCKER' --become
