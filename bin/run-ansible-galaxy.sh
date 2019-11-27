e88e5991f8cc649ab14165602b15b2becc8e85b9
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2019-11-27 09:58:13 +0100

    Add wrapper for ansible-galaxy command

---
+++ bin/run-ansible-galaxy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
+
+set -eo pipefail
+
+# shellcheck source=init-env.sh
+source "$THIS_DIR/init-env.sh"
+
+run-ansible-galaxy "$@"
