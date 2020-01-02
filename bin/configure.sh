14ad63272773e732a058287e1a000e08f54b9cbc
Dmitri Rubinstein <dmitri.rubinstein@googlemail.com>
2020-01-02 15:34:51 +0100

    Detect python executable
    Required in the case 'python' executable is missing,
    but e.g. 'python3' is installed by default (e.g. on CentOS 8)

--- bin/configure.sh
+++ bin/configure.sh
@@ -7,4 +7,5 @@ THIS_DIR=$( (cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) )
 # shellcheck source=init-env.sh
 source "$THIS_DIR/init-env.sh"
 
-"$THIS_DIR/configure.py" "$@"
+set -x
+"$PY" "$THIS_DIR/configure.py" "$@"
